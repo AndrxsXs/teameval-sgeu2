054f4c033c8dbd1a10d1fb4446f0911b6ccb7b00
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-04-15 18:19:42 -0500

    Creación de páginas para autenticación

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,16 @@
-import react from 'react'
+import React from 'react';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 function App() {
 
   return (
     <>
-    
+    <Login></Login>
+    <Register></Register>
+    <Home></Home>
     </>
   )
 }
