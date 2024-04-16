53e0b38fa145380e5fae8449f9d331bfa2ad6a83
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-04-15 20:56:52 -0500

    Ajustes en la nueva paleta de colores

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,9 +1,14 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+import { CssVarsProvider } from '@mui/joy/styles';
+import theme from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+
+    <CssVarsProvider theme={theme} disableTransitionOnChange>
+      <App />
+    </CssVarsProvider>
   </React.StrictMode>,
 )
