3ed16d3a45cf296967993b0be669c78dc95b8927
Ana Maria Montaño Ospina <ana.montano@correounivalle.edu.co>
2024-04-18 21:32:28 -0500

    implementacion 404

--- frontend/src/pages/NotFound.jsx
+++ frontend/src/pages/NotFound.jsx
@@ -1,5 +1,17 @@
-export default function NotFound() {
-    return(
-        <div> 404 </div>
-    )
+import imagen_404 from '../images/404..png'
+import Button from '@mui/joy/Button';
+
+function NotFound() {
+    return (
+        <>
+        <img style={{width:"100%", aspectRatio:"1500/511"}}
+         src={imagen_404} alt="Error 404: pagina no encontrada"/>  
+
+        <div style={{ display: 'flex', justifyContent: 'center', marginTop: '20px' }}>
+            <Button variant="outlined">Volver a la página principal</Button>
+        </div>
+      </>
+    );
 }
+
+export default NotFound
