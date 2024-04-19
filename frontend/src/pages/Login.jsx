67db0ceed6548ebe064d53ab66f0b8fff18844ef
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-04-18 21:34:03 -0500

    Creación de vista de login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -9,68 +9,69 @@ import Input from '@mui/joy/Input';
 import Button from '@mui/joy/Button';
 import Link from '@mui/joy/Link';
 import ModeToggle from '../components/ModeToggle';
+import '../styles/pages/Login.css'
+import TopNavbar from '../components/TopNavbar';
 
 function App() {
 
   return (
-    <main>
-      <CssBaseline/>
-      <CssVarsProvider>
-        <Sheet variant="outlined" sx={{
-          width: 300,
-          mx: 'auto', // margin left & right
-          my: 4, // margin top & bottom
-          py: 3, // padding top & bottom
-          px: 2, // padding left & right
-          display: 'flex',
-          flexDirection: 'column',
-          gap: 2,
-          borderRadius: 'sm',
-          boxShadow: 'md',
-        }}
-        >
-          <div>
-            <Typography level="h4" component="h1">
-              Hola!
-            </Typography>
-            <Typography level="body-sm">
-              Inicia sesión para continuar
-            </Typography>
-            <ModeToggle />
-          </div>
-          <FormControl>
-            <FormLabel>Email</FormLabel>
-            <Input
-              // html input attribute
-              name="email"
-              type="email"
-              placeholder="johndoe@email.com"
-              required
-            />
-          </FormControl>
-          <FormControl>
-            <FormLabel>Contraseña</FormLabel>
-            <Input
-              name="password"
-              type="password"
-              placeholder="Ingresa tu contraseña"
-              required
-            />
-            <Button type="submit" sx={{ my: 1 }}>
-              Iniciar sesión
-            </Button>
-            <Typography
-              endDecorator={<Link href="/#">Regístrate</Link>}
-              fontSize="sm"
-              sx={{ alignSelf: 'center' }}
-            >
-              ¿No tienes una cuenta?
-            </Typography>
-          </FormControl>
-
-        </Sheet>
-      </CssVarsProvider>
-    </main>
+    <>
+      <TopNavbar />
+      <main>
+        <CssBaseline />
+        <CssVarsProvider>
+          <Sheet variant="outlined" sx={{
+            width: 300,
+            mx: 'auto', // margin left & right
+            my: 4, // margin top & bottom
+            py: 3, // padding top & bottom
+            px: 2, // padding left & right
+            display: 'flex',
+            flexDirection: 'column',
+            gap: 2,
+            borderRadius: 'sm',
+            boxShadow: 'md',
+          }}
+          >
+            <div>
+              <Typography level="h4" component="h1">
+                Le damos la bienvenida
+              </Typography>
+              <Typography level="body-sm">
+                Inicie sesión para continuar
+              </Typography>
+            </div>
+            <FormControl>
+              <FormLabel>Correo electrónico</FormLabel>
+              <Input
+                // html input attribute
+                name="email"
+                type="email"
+                placeholder="nombre@correo.com"
+                required
+              />
+            </FormControl>
+            <FormControl>
+              <FormLabel>Contraseña</FormLabel>
+              <Input
+                name="password"
+                type="password"
+                placeholder="Ingrese su contraseña"
+                required
+              />
+              <Button type="submit" sx={{ my: 1 }}>
+                Iniciar sesión
+              </Button>
+              <Typography sx={{ alignSelf: 'center' }}>
+                <Link href="/login"
+                  fontSize="sm">
+                  Olvidé mi contraseña
+                </Link></Typography>
+            </FormControl>
+          </Sheet>
+        </CssVarsProvider>
+      </main >
+    </>
   )
 }
 
