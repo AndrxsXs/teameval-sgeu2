bec4bdeeb59287f887ab21daf8268115f9296996
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-05-08 10:58:01 -0500

    fix: error en ancho del container

--- frontend/src/pages/AdminPage.jsx
+++ frontend/src/pages/AdminPage.jsx
@@ -1,25 +1,21 @@
 import { CssVarsProvider } from "@mui/joy/styles"
 import CssBaseline from "@mui/joy/CssBaseline"
+import { Routes, Route } from 'react-router-dom';
 import AdminSidebar from "../components/AdminSidebar"
+import ManageTeachers from "../pages/admin/ManageTeachers"
 import "../styles/pages/AdminPage.css"
-import Typography from '@mui/joy/Typography'
 import Box from '@mui/material/Box'
-import Button from '@mui/joy/Button'
-import PersonAddRounded from '@mui/icons-material/PersonAddRounded';
+import ManageAdmin from "./admin/ManageAdmin";
+import ManageCourses from "./admin/ManageCourses";
+import ManageScales from "./admin/ManageScales";
 
 function AdminPage() {
     return (
         <>
             <CssVarsProvider disableTransitionOnChange>
-                <Box className="container" sx={{}}>
+                <Box className="container" sx={{width: '100%'}}>
                     <CssBaseline />
                     <AdminSidebar />
-                    {/* <main className="main-container">
-                <Typography level="h1" color="initial">
-                    Contenido de la página de administrador
-                </Typography>
-
-            </main> */}
 
                     <Box component="main" className="main-container"
                         sx={{
@@ -33,47 +29,19 @@ function AdminPage() {
                             flex: 1,
                             display: 'flex',
                             flexDirection: 'column',
-                            minWidth: 0,
                             height: '100dvh',
+                            width: '100%',
                             gap: 1,
-
-                            // flex: 1,
-                            // display: 'flex',
-                            // m: 0,
-                            // p: 8,
-                            // py: 0,
-                            // flexDirection: 'column',
-                            // alignItems: 'center',
-                            // justifyContent: 'center',
-                            // height: '100dvh',
-                            // width: '100%',
-                            //bgcolor: '#cdbd21',
+                            margin: 'auto 0',
                         }}>
-                        <Box component="header"
-                            sx={{
-                                display: 'flex',
-                                mb: 1,
-                                gap: 1,
-                                flexDirection: { xs: 'column', sm: 'row' },
-                                alignItems: { xs: 'start', sm: 'center' },
-                                flexWrap: 'wrap',
-                                justifyContent: 'space-between',
-                            }}
-                        >
-                            <Typography level="h2" component="h1">
-                                Configuración de docentes
-                            </Typography>
 
-                            <Button
-                                color="primary"
-                                startDecorator={<PersonAddRounded />}
-                                size="sm"
-                            >
-                                Agregar docente
-                            </Button>
+                        <Routes>
+                            <Route path='/manage/admin' element={<ManageAdmin />} />
+                            <Route path='/manage/teachers' element={<ManageTeachers />} />
+                            <Route path='/manage/courses' element={<ManageCourses />} />
+                            <Route path='/manage/scales' element={<ManageScales />} />
+                        </Routes>
 
-
-                        </Box>
                     </Box>
                 </Box>
             </CssVarsProvider >
