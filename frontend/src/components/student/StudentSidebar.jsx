1e89b72cb62dcfb649a4d05cbbf23eccdcafd0c1
Ana Maria Montaño Ospina <ana.montano@correounivalle.edu.co>
2024-05-11 17:23:23 -0500

    Creación sidebar estudiante.

--- frontend/src/components/student/StudentSidebar.jsx
+++ frontend/src/components/student/StudentSidebar.jsx
@@ -1,8 +1,35 @@
+import Sidebar from '../Sidebar';
+import SupervisorAccountRoundedIcon from '@mui/icons-material/SupervisorAccountRounded';
+import AutoStoriesRoundedIcon from '@mui/icons-material/AutoStoriesRounded';
+import EditNoteRoundedIcon from '@mui/icons-material/EditNoteRounded';
+import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
+
 export default function StudentSidebar() {
-    
-    return(
-        <>
-        
-        </>
+
+    const menuItems = [
+        {
+            text: 'Evaluaciones',
+            route: '/student',
+            icon: <SupervisorAccountRoundedIcon key="student" />,
+        },
+        {
+            text: 'Profesores',
+            route: '/student/result',
+            icon: <PersonRoundedIcon key="result" />,
+        },
+        {
+            text: 'Cursos',
+            route: '/student/feedback',
+            icon: <AutoStoriesRoundedIcon key="feedback" />,
+        },
+    ];
+    const settingsRoute = '/student/settings';
+
+    return (
+        <Sidebar
+            firstHeader="Gestión"
+            menuItems={menuItems}
+            settingsRoute={settingsRoute}
+        />
     )
 }
