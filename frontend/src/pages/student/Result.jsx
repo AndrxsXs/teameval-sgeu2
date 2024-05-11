2c1101fcf5173f22a3ef033661ab097b9c7bba34
Ana Maria Montaño Ospina <ana.montano@correounivalle.edu.co>
2024-05-11 17:23:44 -0500

    Creación titulos para cada ventana.

--- frontend/src/pages/student/Result.jsx
+++ frontend/src/pages/student/Result.jsx
@@ -1,7 +1,32 @@
+import { Box } from "@mui/joy"
+import { Typography } from "@mui/joy"
 
-export default function Result () {
-    return(
 
-        <h1>k</h1>
+export default function Result() {
+
+    return (
+        <>
+            <Box component="header"
+                sx={{
+                    display: 'flex',
+                    mt: 2,
+                    mb: 1,
+                    gap: 1,
+                    flexDirection: { xs: 'column', sm: 'row' },
+                    alignItems: { xs: 'start', sm: 'center' },
+                    flexWrap: 'wrap',
+                    justifyContent: 'space-between',
+                    width: '100%',
+                }}
+            >
+                <Typography level="h2" component="h1">
+                    Resultados disponibles
+                </Typography>
+
+                
+            </Box>
+            
+            
+        </>
     )
 }
