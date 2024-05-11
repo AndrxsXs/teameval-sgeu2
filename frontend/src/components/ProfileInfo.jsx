9732d683fc8bed559bc20517a072503bf00d44ba
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-05-11 15:16:21 -0500

    fix: diseño en info del usuario

--- frontend/src/components/ProfileInfo.jsx
+++ frontend/src/components/ProfileInfo.jsx
@@ -1,33 +1,62 @@
-import { useContext } from 'react';
+import { useContext, useState, useEffect } from 'react';
 import { AuthContext } from '../components/auth/AuthContext';
 
 import Avatar from '@mui/joy/Avatar';
 import Box from '@mui/joy/Box';
 import Typography from '@mui/joy/Typography';
-import IconButton from '@mui/joy/IconButton';
-import LogoutRoundedIcon from '@mui/icons-material/LogoutRounded';
+import LogoutButton from './auth/LogoutButton';
+import Skeleton from '@mui/joy/Skeleton';
 
 export default function ProfileInfo() {
     const { user } = useContext(AuthContext);
+    const [loading, setLoading] = useState(!user); // Establecer loading a true inicialmente
 
-    if (!user) {
-        return <div>Cargando...</div>; // O cualquier otro componente de carga que prefieras
-    }
+    useEffect(() => {
+        // Simular un retraso de 2 segundos para obtener los datos del usuario
+        const timeout = setTimeout(() => {
+            setLoading(false); // Establecer loading a false después de obtener los datos del usuario
+        }, 2000);
+
+        return () => clearTimeout(timeout); // Limpiar el timeout en el desmontaje
+    }, [user]); // El arreglo vacío asegura que el efecto se ejecute una sola vez
 
     return (
-        <Box component="section" sx={{ display: 'flex', gap: 1, alignItems: 'center', p: '16px' }}>
+        <Box component="section" sx={{
+            display: 'flex',
+            flexWrap: 'nowrap',
+            justifyContent: 'space-between',
+            alignItems: 'center',
+            gap: 1,
+            p: '16px'
+        }}>
             <Avatar
                 variant="outlined"
                 size="sm"
-                src="https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?auto=format&fit=crop&w=286"
+                src=""
             />
-            <Box sx={{ minWidth: 0, flex: 1 }}>
-                <Typography level="title-sm">{`${user.name} ${user.lastName}`}</Typography>
-                <Typography level="body-xs">{user.email}</Typography>
+            <Box sx={{
+                //minWidth: 0,
+                display: 'flex',
+                // flex: 1, 
+                width: '100%',
+                flexDirection: 'column',
+                alignContent: 'flex-start',
+                justifyContent: 'flex-start',
+                alignItems: 'flex-start',
+                flexWrap: 'nowrap',
+            }}>
+                <Typography level="title-sm">
+                    <Skeleton loading={loading} animation="wave">
+                        {user && `${user.name} ${user.lastName}`}
+                    </Skeleton>
+                </Typography>
+                <Typography level="body-xs">
+                    <Skeleton loading={loading} animation="wave">
+                        {user && user.email}
+                    </Skeleton>
+                </Typography>
             </Box>
-            <IconButton size="sm" variant="plain" color="neutral" component="a" href="/logout">
-                <LogoutRoundedIcon />
-            </IconButton>
+            <LogoutButton />
         </Box>
     );
 }
