04fa412009c5af980cc6c2c28b7244b15a5ab9a5
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-05-16 21:43:34 -0500

    chore: Add DisableUser component for disabling users
    This commit adds a new component called DisableUser, which allows for disabling a user. The component includes a button that opens a modal dialog confirming the user's disablement. It also handles the logic for disabling the user and provides a cancel option. This component will be useful for managing user accounts and their status.

---
+++ frontend/src/components/DisableUser.jsx
@@ -0,0 +1,88 @@
+/* eslint-disable react/prop-types */
+import { Fragment, useState } from 'react';
+
+import {
+    Button,
+    Box,
+    Typography
+
+} from '@mui/joy';
+
+import ModalFrame from '../components/ModalFrame';
+
+export default function DisableUser(props) {
+
+    const { user } = props;
+
+    const [loading, setLoading] = useState(false);
+    const [isModalOpen, setIsModalOpen] = useState(false);
+
+    const handleOpenModal = () => {
+        setIsModalOpen(true);
+    }
+
+    const handleCloseModal = (value) => {
+        setIsModalOpen(value);
+    }
+    return (
+        <Fragment>
+            <Button
+                size="sm"
+                variant="soft"
+                color="danger"
+                onClick={handleOpenModal}
+            >
+                Deshabilitar
+            </Button>
+            <ModalFrame
+                open={isModalOpen}
+                onClose={handleCloseModal}
+                ModalTitle="Deshabilitar usuario"
+            >
+                <Typography id="nested-modal-description" textColor="text.tertiary"
+                    sx={{
+                        minWidth: '500px',
+                        minHeight: '50px'
+                    }}
+                >
+                    Está a punto de deshabilitar a <strong>{user.name} {user.last_name}</strong>.
+                    {/* <br />
+                    El usuario no podrá acceder a su cuenta.
+                    <br />
+                    Se puede {' '}<strong>
+                    <Typography color='success'>
+                        habilitar
+                    </Typography>
+                    </strong> {' '}
+                    nuevamente en cualquier momento. */}
+                </Typography>
+                <Box
+                    sx={{
+                        mt: 1,
+                        display: 'flex',
+                        gap: 1,
+                        flexDirection: { xs: 'column', sm: 'row-reverse' },
+                    }}
+                >
+
+                    <Button variant="solid"
+                        color="danger"
+
+                        loading={loading}
+                        onClick={() => { handleOpenModal; setLoading(true) }}>
+                        Deshabilitar
+                    </Button>
+
+                    <Button
+                        variant="outlined"
+                        color="neutral"
+                        onClick={() => handleCloseModal(false)}
+                    >
+                        Cancelar
+                    </Button>
+
+                </Box>
+            </ModalFrame>
+        </Fragment>
+    )
+}
