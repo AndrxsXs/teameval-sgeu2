85b3da18d0f576488a9413475608d1bb6a275759
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-05-16 21:40:29 -0500

    chore: Update main.jsx to remove AuthProvider
    This commit removes the AuthProvider component from the main.jsx file, as it is no longer needed for the application.

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,17 +1,14 @@
 // import React from 'react'
 import { BrowserRouter } from 'react-router-dom';
-import { AuthProvider } from './components/auth/AuthContext.jsx';
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import { CssVarsProvider } from '@mui/joy/styles';
 import theme from './styles/theme';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <AuthProvider>
     <BrowserRouter>
       <CssVarsProvider theme={theme} disableTransitionOnChange>
         <App />
       </CssVarsProvider>
     </BrowserRouter>
-  </AuthProvider>
 )
