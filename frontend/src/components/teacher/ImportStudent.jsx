39f606a53242a06be1352281ada438b559a630be
Ana Maria Montaño Ospina <ana.montano@correounivalle.edu.co>
2024-05-19 15:40:53 -0500

    Vista del importar estudiantes.

--- frontend/src/components/teacher/ImportStudent.jsx
+++ frontend/src/components/teacher/ImportStudent.jsx
@@ -1,11 +1,45 @@
-import * as React from 'react';
+import React from 'react';
+import { Box, Button, Typography } from '@mui/joy';
+import UploadIcon from '@mui/icons-material/Upload';
 
-
-export default function ImportStudent() {
+const ImportStudent = ({ handleClose }) => {
   return (
-    
-      <h1>Hola</h1>
-  
-  );
-}
+    <>
+      <Typography variant="h6" sx={{ fontWeight: 'bold', marginBottom: 2 }}>
+        Recursos
+      </Typography>
+      <Box sx={{ border: '1px solid gray', padding: 2 }}>
 
+        <Typography variant="body2" sx={{ marginBottom: 2 }}>
+          Compartir recursos para el mejoramiento del trabajo en equipo
+        </Typography>
+        <Box
+          sx={{
+            border: '1px solid lightgray',
+            padding: '16px',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            borderRadius: '4px',
+            marginBottom: 2,
+          }}
+        >
+          <UploadIcon sx={{ color: 'blue', marginRight: '8px' }} />
+          <Typography variant="body2">
+            Haga clic para actualizar o arrastre y suelte.
+          </Typography>
+        </Box>
+        <Box sx={{ display: 'flex', justifyContent: 'space-between' }}>
+          <Button variant="outlined" color="neutral" sx={{ alignSelf: 'center' }} onClick={handleClose}>
+            Cancelar
+          </Button>
+          <Button sx={{ alignSelf: 'center', color: 'white' }} type="submit" >
+            Enviar
+          </Button>
+        </Box>
+      </Box>
+    </>
+  );
+};
+
+export default ImportStudent;
