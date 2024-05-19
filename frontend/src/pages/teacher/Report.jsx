741991ac65f0e374028da6a15fbf5c0f07ef80b5
Ana Maria Montaño Ospina <ana.montano@correounivalle.edu.co>
2024-05-18 23:41:26 -0500

    primer boceto import

--- frontend/src/pages/teacher/Report.jsx
+++ frontend/src/pages/teacher/Report.jsx
@@ -1,10 +1,60 @@
+import React from 'react';
+import { Box, Button, Typography } from '@mui/joy';
+import UploadIcon from '@mui/icons-material/Upload';
 
 
-export default function Report() {
-
+const Report = () => {
     return (
         <>
-            <h1>asdfdsgdhgf</h1>
+            <Box sx={{ border: '1px solid gray', padding: 2 }}>
+                <Typography variant="h6" sx={{ fontWeight: 'bold', marginBottom: 2 }}>
+                    Recursos
+                </Typography>
+                
+                <Typography variant="body2" sx={{ marginBottom: 2 }}>
+                    Compartir recursos para el mejoramiento del trabajo en equipo
+                </Typography>
+
+                <Box
+                    sx={{
+                        border: '1px solid lightgray',
+                        padding: '16px',
+                        display: 'flex',
+                        justifyContent: 'center',
+                        alignItems: 'center',
+                        borderRadius: '4px',
+                        marginBottom: 2,
+                    }}
+                >
+                    <UploadIcon sx={{ color:'blue', marginRight:'8px' }}/>
+                    <Typography variant="body2">
+                        Haga clic para actualizar o arrastre y suelte.
+                    </Typography>
+                </Box>
+
+                <Box sx={{ display:'flex', justifyContent:'space-between' }}>
+                    <Button
+                        variant="text"
+                        sx={{
+                            textTransform:'none'
+                         }}
+                     >
+                         Cancelar
+                     </Button>
+                     
+                     <Button 
+                         variant="contained" 
+                         color="primary"
+                         sx={{
+                             textTransform:'none'
+                          }}
+                      >
+                          Guardar
+                      </Button>
+                 </Box>
+            </Box>
         </>
-    )
-}
+    );
+};
+
+export default Report;
