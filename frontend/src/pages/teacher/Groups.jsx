fe713663936d6311681808e5177b697332390b84
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-05-23 18:51:56 -0500

    feat: Mejorar página de Grupos
    Se han realizado mejoras en la página de Grupos. Los cambios incluyen:
    
    - Actualizar la carga de la página con carga perezosa para las imágenes.
    - Agregar el componente CreateGroup para crear nuevos grupos.
    - Mejorar el diseño y la disposición de los elementos en la página.
    
    Estos cambios mejoran la funcionalidad y la apariencia de la página de Grupos.

--- frontend/src/pages/teacher/Groups.jsx
+++ frontend/src/pages/teacher/Groups.jsx
@@ -1,50 +1,43 @@
-import FileUploadIcon from '@mui/icons-material/FileUpload';
-import { Box } from "@mui/joy"
-import { Typography } from "@mui/joy"
-import { Button } from "@mui/joy"
-import Add from '@mui/icons-material/Add';
+import FileUploadIcon from "@mui/icons-material/FileUpload";
+import { Box } from "@mui/joy";
+import { Typography } from "@mui/joy";
+import { Button } from "@mui/joy";
+import Add from "@mui/icons-material/Add";
 import GroupsTable from "../../components/teacher/groups/GroupsTable";
+import CreateGroup from "../../components/teacher/groups/CreateGroup";
 
 export default function Groups() {
+  return (
+    <>
+      <Box
+        component="header"
+        sx={{
+          display: "flex",
+          mt: 2,
+          mb: 1,
+          gap: 1,
+          flexDirection: { xs: "column", sm: "row" },
+          alignItems: { xs: "start", sm: "center" },
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+          width: "100%",
+        }}
+      >
+        <Typography level="h2" component="h1">
+          Grupos
+        </Typography>
 
-    return (
-        <>
-              <Box component="header"
-                sx={{
-                    display: 'flex',
-                    mt: 2,
-                    mb: 1,
-                    gap: 1,
-                    flexDirection: { xs: 'column', sm: 'row' },
-                    alignItems: { xs: 'start', sm: 'center' },
-                    flexWrap: 'wrap',
-                    justifyContent: 'space-between',
-                    width: '100%',
-                }}
-            >
-                <Typography level="h2" component="h1">
-                    Grupos
-                </Typography>
-
-                <Box
-                sx={{
-
-                   display: 'flex',
-                   gap: 1,
-                   flexDirection: { xs: 'column', sm: 'row' },                    
-                }}
-                >
-                    <Button
-                        color="primary"
-                        startDecorator={<Add />}
-                        size="sm"
-                    >
-                        Crear grupos
-                    </Button>
-                    
-                </Box>
-                <GroupsTable/>
-            </Box>
-        </>
-    )
+        <Box
+          sx={{
+            display: "flex",
+            gap: 1,
+            flexDirection: { xs: "column", sm: "row" },
+          }}
+        >
+          <CreateGroup />
+        </Box>
+        <GroupsTable />
+      </Box>
+    </>
+  );
 }
