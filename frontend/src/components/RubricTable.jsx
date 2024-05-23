7dd349b5faa7e473fb4d6e94fe3f8eb3ef5701db
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-05-23 15:37:15 -0500

    feat: Add RubricTable component
    A new RubricTable component has been added to the project. This component is responsible for rendering a table for rubric data.

---
+++ frontend/src/components/RubricTable.jsx
@@ -0,0 +1,21 @@
+import {
+    Fragment,
+    // useEffect,
+    // useState,
+} from 'react';
+
+// import api from '../api';
+
+import {
+    
+} from '@mui/joy';
+
+export default function RubricTable() {
+
+
+    return (
+        <Fragment>
+
+        </Fragment>
+    )
+}
