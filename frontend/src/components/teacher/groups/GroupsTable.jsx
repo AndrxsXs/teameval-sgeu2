8c1c5e832d2f9b2cc920206e993f76dd5f567775
Ana Maria Montaño Ospina <ana.montano@correounivalle.edu.co>
2024-05-23 15:06:29 -0500

    Tabla donde se visualizan los grupos que se crean

---
+++ frontend/src/components/teacher/groups/GroupsTable.jsx
@@ -0,0 +1,14 @@
+import UserGroupTable from "./UserGroupTable"
+
+export default function AdminTable() {
+
+    const columns = ["Id", "Nombre grupo", "Nombre proyecto", "Número de integrantes"];
+
+    return (
+        <UserGroupTable
+            role={1} 
+            columns={columns}
+        />
+    )
+}
+
