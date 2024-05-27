cf646879a835d692a0b66a389f291c3d133521be
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-05-27 18:29:12 -0500

    refactor: Update AddStudent component layout and styles

--- frontend/src/pages/teacher/course/AddStudent.jsx
+++ frontend/src/pages/teacher/course/AddStudent.jsx
@@ -1,48 +1,46 @@
 import { Box, Typography } from "@mui/joy";
 import CreateStudent from "../../../components/teacher/CreateStudent";
 import BodyAddStudent from "../../../components/teacher/BodyAddStudent";
-import { useState } from 'react';
-import { Button, Modal, Sheet } from "@mui/joy";
-import FileUploadIcon from '@mui/icons-material/FileUpload';
+import { useState } from "react";
 import ImportStudent from "../../../components/teacher/ImportStudent";
 
 export default function AddStudent() {
-    const [isModalOpen, setIsModalOpen] = useState(false);
+  const [isModalOpen, setIsModalOpen] = useState(false);
 
-    const handleOpenModal = () => setIsModalOpen(true);
-    const handleCloseModal = () => setIsModalOpen(false);
+  const handleOpenModal = () => setIsModalOpen(true);
+  const handleCloseModal = () => setIsModalOpen(false);
 
-    return (
-        <>
-            <Box
-                component="header"
-                sx={{
-                    display: 'flex',
-                    mt: 2,
-                    mb: 1,
-                    gap: 1,
-                    flexDirection: { xs: 'column', sm: 'row' },
-                    alignItems: { xs: 'start', sm: 'center' },
-                    flexWrap: 'wrap',
-                    justifyContent: 'space-between',
-                    width: '100%',
-                }}
-            >
-                <Typography level="h2" component="h1">
-                    Nombre del curso
-                </Typography>
-                <Box
-                    sx={{
-                        display: 'flex',
-                        gap: 1,
-                        flexDirection: { xs: 'column', sm: 'row' },
-                    }}
-                >
-                    <CreateStudent />
-                    <ImportStudent />
-                </Box>
-                <BodyAddStudent />
-            </Box>
-        </>
-    );
+  return (
+    <>
+      <Box
+        component="header"
+        sx={{
+          display: "flex",
+          mt: 2,
+          mb: 1,
+          gap: 1,
+          flexDirection: { xs: "column", sm: "row" },
+          alignItems: { xs: "start", sm: "center" },
+          flexWrap: "wrap",
+          justifyContent: "space-between",
+          width: "100%",
+        }}
+      >
+        <Typography level="h2" component="h1">
+          Nombre del curso
+        </Typography>
+        <Box
+          sx={{
+            display: "flex",
+            gap: 1,
+            flexDirection: { xs: "column", sm: "row" },
+          }}
+        >
+          <CreateStudent />
+          <ImportStudent />
+        </Box>
+      </Box>
+      <BodyAddStudent />
+    </>
+  );
 }
