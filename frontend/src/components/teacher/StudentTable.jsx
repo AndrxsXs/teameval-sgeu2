20d990b8d3b4e3369df4ac82b56af48aa7788338
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-05-27 18:26:01 -0500

    refactor: Update column names in StudentTable component
    This commit updates the column names in the StudentTable component. The "Cédula" column has been changed to "Código" to better reflect the data being displayed. Additionally, the "role" prop has been updated from 3 to 1 to ensure the correct role is passed to the UserTable component.

--- frontend/src/components/teacher/StudentTable.jsx
+++ frontend/src/components/teacher/StudentTable.jsx
@@ -3,12 +3,12 @@ import UserTable from '../UserTable';
 export default function StudentTable() {
 
     const columns = [
-        "Cédula", "Nombre", "Correo electrónico", "Estado", "Acciones"
+        "Código", "Nombre", "Correo electrónico", "Estado", "Acciones"
     ]
 
     return (
         <UserTable
-            role={3}
+            role={1}
             columns={columns}
         />
     )
