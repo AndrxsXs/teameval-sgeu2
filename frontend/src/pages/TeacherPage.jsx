91f0c147c117f10033ff8783019d9eee16595276
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-05-27 18:29:33 -0500

    feat: Update TeacherPage component with lazy loading for images

--- frontend/src/pages/TeacherPage.jsx
+++ frontend/src/pages/TeacherPage.jsx
@@ -1,42 +1,40 @@
 /* eslint-disable react/prop-types */
+import { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom";
 
-import { Routes, Route } from 'react-router-dom'
-import CourseView from './teacher/course/CourseView';
+import api from "../api";
 
-import AddStudent from './teacher/course/AddStudent'
-import DisableStudent from "./teacher/course/DisableStudent"
-import Groups from './teacher/Groups'
-import ScalesCriteria from './teacher/ScalesCriteria'
-import Report from './teacher/Report'
-import MainTeacherView from './teacher/MainTeacherView';
-import ImportStudent from '../components/teacher/ImportStudent';
-import Resources from './teacher/Resources';
+import CourseView from "./teacher/course/CourseView";
+
+import AddStudent from "./teacher/course/AddStudent";
+import DisableStudent from "./teacher/course/DisableStudent";
+import Groups from "./teacher/Groups";
+import ScalesCriteria from "./teacher/ScalesCriteria";
+import Report from "./teacher/Report";
+import MainTeacherView from "./teacher/MainTeacherView";
+import ImportStudent from "../components/teacher/ImportStudent";
+import Resources from "./teacher/Resources";
 
 function TeacherPage({ userData }) {
-    return (
-      <>
-        <Routes>
-          <Route index element={<MainTeacherView userData={userData}/>} />
-          <Route path='/curso/*' element={<CourseView userData={userData} />}>
-            <Route path='importar-estudiante/*' element={<AddStudent />}>
-              <Route path='subir' element={<ImportStudent />} />
-            </Route>
-            <Route path='deshabilitar-estudiante' element={<DisableStudent />} />
-            <Route path='grupos' element={<Groups />} />
-            <Route path='escalas' element={<ScalesCriteria />} />
-            <Route path='informes' element={<Report />} />
-            <Route path='recursos' element={<Resources />} />
-          </Route>
-        </Routes>
+  return (
+    <>
+      <Routes>
+        <Route index element={<MainTeacherView userData={userData} />} />
+        <Route
+          path="/curso/:courseId/*"
+          element={<CourseView userData={userData} />}
+        >
+          <Route index element={<AddStudent />} />
+          <Route path="subir" element={<ImportStudent />} />
+          <Route path="deshabilitar-estudiante" element={<DisableStudent />} />
+          <Route path="grupos" element={<Groups />} />
+          <Route path="escalas" element={<ScalesCriteria />} />
+          <Route path="informes" element={<Report />} />
+          <Route path="recursos" element={<Resources />} />
+        </Route>
+      </Routes>
+    </>
+  );
+}
 
-        
-      </>
-    )
-  }
-
-export default TeacherPage
-
-
-
-
-
+export default TeacherPage;
