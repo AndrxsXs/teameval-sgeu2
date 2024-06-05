01c09e1d4a0ad694801997a57af45ff3cc835103
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-06-05 00:53:42 -0500

    chore: Add cally library for call functionality

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,39 +1,54 @@
-import { Link } from "react-router-dom"
-import TopNavbar from "../components/TopNavbar"
-import Typography from "@mui/joy/Typography"
-import PhotoHomePage from '../assets/photoHomePage.png'
-import '../styles/pages/HomePage.css'
+import { Link } from "react-router-dom";
+import TopNavbar from "../components/TopNavbar";
+import Typography from "@mui/joy/Typography";
+import PhotoHomePage from "../assets/photoHomePage.png";
+import "../styles/pages/HomePage.css";
 
-import Button from "@mui/joy/Button"
-import { Box } from "@mui/joy"
+import Button from "@mui/joy/Button";
+import Box from "@mui/joy/Box";
+// import Stack from "@mui/joy/Stack";
+import { CssVarsProvider } from "@mui/joy";
+
+import { Fragment } from "react";
 
 export default function HomePage() {
-    return (
-        <>
-            <TopNavbar />
-            <Box component="main" className="main-homepage">
-                <article className="hero">
-                    <section className="presentation">
-                        <Typography level="title-lg" color="primary">
-                            El poder de hacer más
-                        </Typography>
-                        <Typography level="h1">
-                            La mejor manera de evaluar el talento
-                        </Typography>
-                        <Typography level="body-md">
-                            Con TeamEval puedes agilizar la forma en que evalúas a tus compañeros de equipo.
-                        </Typography>
-                        <div>
-                            <Link to="/login/">
-                                <Button>Entrar</Button>
-                            </Link>
-                        </div>
-                    </section>
-                    <section>
-                        <img className="img-home" src={PhotoHomePage} alt="" />
-                    </section>
-                </article>
-            </Box>
-        </>
-    )
+  return (
+    <Fragment>
+      <CssVarsProvider
+        disableTransitionOnChange
+        // direction="column"
+        // justifyContent="center"
+        // sx={{
+        //   height: "100vh",
+        //   width: "100%",
+        // }}
+      >
+        <TopNavbar />
+        <Box component="main" className="main-homepage">
+          <article className="hero">
+            <section className="presentation">
+              <Typography level="title-lg" color="primary">
+                El poder de hacer más
+              </Typography>
+              <Typography level="h1">
+                La mejor manera de evaluar el talento
+              </Typography>
+              <Typography level="body-md">
+                Con TeamEval puedes agilizar la forma en que evalúas a tus
+                compañeros de equipo.
+              </Typography>
+              <div>
+                <Link to="/login/">
+                  <Button>Entrar</Button>
+                </Link>
+              </div>
+            </section>
+            <section>
+              <img className="img-home" src={PhotoHomePage} alt="" />
+            </section>
+          </article>
+        </Box>
+      </CssVarsProvider>
+    </Fragment>
+  );
 }
--- frontend/src/pages/TeacherPage.jsx
+++ frontend/src/pages/TeacherPage.jsx
@@ -1,18 +1,14 @@
 /* eslint-disable react/prop-types */
-import { useState, useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
-
-import api from "../api";
 
 import CourseView from "./teacher/course/CourseView";
 
 import AddStudent from "./teacher/course/AddStudent";
-import DisableStudent from "./teacher/course/DisableStudent";
 import Groups from "./teacher/Groups";
 import ScalesCriteria from "./teacher/ScalesCriteria";
 import Report from "./teacher/Report";
 import MainTeacherView from "./teacher/MainTeacherView";
-import ImportStudent from "../components/teacher/ImportStudent";
+// import ImportStudent from "../components/teacher/ImportStudent";
 import Resources from "./teacher/Resources";
 
 function TeacherPage({ userData }) {
@@ -25,8 +21,6 @@ function TeacherPage({ userData }) {
           element={<CourseView userData={userData} />}
         >
           <Route index element={<AddStudent />} />
-          <Route path="subir" element={<ImportStudent />} />
-          <Route path="deshabilitar-estudiante" element={<DisableStudent />} />
           <Route path="grupos" element={<Groups />} />
           <Route path="escalas" element={<ScalesCriteria />} />
           <Route path="informes" element={<Report />} />
