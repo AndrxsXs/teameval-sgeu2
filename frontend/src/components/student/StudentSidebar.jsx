3807689cf7d53ed48dfab30c4870bbc5af89bbd2
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-06-10 19:36:31 -0500

    feat: Actualizar rutas en la barra lateral del estudiante

--- frontend/src/components/student/StudentSidebar.jsx
+++ frontend/src/components/student/StudentSidebar.jsx
@@ -1,7 +1,7 @@
 /* eslint-disable react/prop-types */
 import Sidebar from '../Sidebar';
 import SupervisorAccountRoundedIcon from '@mui/icons-material/SupervisorAccountRounded';
-import AutoStoriesRoundedIcon from '@mui/icons-material/AutoStoriesRounded';
+// import AutoStoriesRoundedIcon from '@mui/icons-material/AutoStoriesRounded';
 // import EditNoteRoundedIcon from '@mui/icons-material/EditNoteRounded';
 import PersonRoundedIcon from '@mui/icons-material/PersonRounded';
 
@@ -10,19 +10,19 @@ export default function StudentSidebar({ userData }) {
     const menuItems = [
         {
             text: 'Evaluaciones',
-            route: '/estudiante',
-            icon: <SupervisorAccountRoundedIcon key="student" />,
+            route: './',
+            icon: <SupervisorAccountRoundedIcon key="evaluations" />,
         },
         {
             text: 'Resultados',
-            route: '/estudiante/resultados',
-            icon: <PersonRoundedIcon key="result" />,
+            route: './resultados',
+            icon: <PersonRoundedIcon key="results" />,
         },
-        {
-            text: 'Retroalimentaciones',
-            route: '/estudiante/retroalimentacion',
-            icon: <AutoStoriesRoundedIcon key="feedback" />,
-        },
+        // {
+        //     text: 'Retroalimentaciones',
+        //     route: './estudiante/retroalimentacion',
+        //     icon: <AutoStoriesRoundedIcon key="feedback" />,
+        // },
     ];
     const settingsRoute = 'settings';
 
