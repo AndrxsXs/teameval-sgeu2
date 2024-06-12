a14618f79be2f90de5e60dd9d879b297b9fa6fa8
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-06-11 22:20:03 -0500

    feat: Refactor CreateRubric component to use MUI Joy components and remove unused dependencies

--- frontend/src/components/teacher/CreateRubric.jsx
+++ frontend/src/components/teacher/CreateRubric.jsx
@@ -8,14 +8,9 @@ import Box from "@mui/joy/Box";
 import Input from "@mui/joy/Input";
 import Select from "@mui/joy/Select";
 import Option from "@mui/joy/Option";
-import Textarea from "@mui/joy/Textarea";
 
 import api from "../../api";
 import { useParams } from "react-router-dom";
-
-import Sheet from "@mui/joy/Sheet";
-import Table from "@mui/joy/Table";
-import { Fragment } from "react";
 
 import IconButton from "@mui/joy/IconButton";
 import CloseRounded from "@mui/icons-material/CloseRounded";
@@ -23,6 +18,8 @@ import CloseRounded from "@mui/icons-material/CloseRounded";
 import FormControl from "@mui/joy/FormControl";
 import FormLabel from "@mui/joy/FormLabel";
 import FormHelperText from "@mui/joy/FormHelperText";
+
+import CriteriaTable from "../CriteriaTable";
 
 import Stack from "@mui/joy/Stack";
 
@@ -40,106 +37,6 @@ const headCells = [
     label: "Descripción de la escala",
   },
 ];
-
-function CriteriaTable(props) {
-  const { rows, headCells, onStandardsChange } = props;
-
-  const handleDescriptionChange = (index, value) => {
-    const updatedStandards = [...rows];
-    updatedStandards[index].description = value;
-    onStandardsChange(updatedStandards);
-  };
-
-  const handleScaleDescriptionChange = (index, value) => {
-    const updatedStandards = [...rows];
-    updatedStandards[index].scale_description = value;
-    onStandardsChange(updatedStandards);
-  };
-  return (
-    <Fragment>
-      <Sheet
-        className="TableContainer"
-        variant="outlined"
-        sx={{
-          borderRadius: "sm",
-          boxShadow: "xs",
-        }}
-      >
-        <Table
-          borderAxis="bothBetween"
-          aria-labelledby="Tabla de criterios de la rúbrica"
-          stickyHeader
-          sx={{
-            "--TableCell-headBackground":
-              "var(--joy-palette-background-level1)",
-            "--Table-headerUnderlineThickness": "1px",
-            "--TableRow-hoverBackground":
-              "var(--joy-palette-background-level1)",
-            "--TableCell-paddingY": "0px",
-            "--TableHeader-paddingY": "12px",
-            "--TableCell-paddingX": "0px",
-            "& thead th": {
-              paddingY: "12px",
-              paddingX: "16px",
-            },
-            // "& thead th:nth-of-type(1)": { width: "10%" },
-
-            "--TableRow-stripeBackground": "rgba(0 0 0 / 0.04)",
-          }}
-        >
-          {/* <EnhancedTableHead
-            // numSelected={selected.length}
-            order={order}
-            orderBy={orderBy}
-            // onSelectAllClick={handleSelectAllClick}
-            onRequestSort={handleRequestSort}
-            rowCount={rows.length}
-            headCells={headCells}
-          /> */}
-          <thead>
-            <tr>
-              {headCells.map((headCell) => (
-                <th key={headCell.id}>{headCell.label}</th>
-              ))}
-            </tr>
-          </thead>
-          <tbody>
-            {rows.map((row, index) => (
-              <tr key={index}>
-                <td>
-                  <Textarea
-                    required
-                    size="sm"
-                    variant="plain"
-                    minRows={3}
-                    maxRows={5}
-                    onChange={(e) =>
-                      handleDescriptionChange(index, e.target.value)
-                    }
-                    placeholder="Descripción del criterio"
-                  />
-                </td>
-                <td>
-                  <Textarea
-                  required
-                    size="sm"
-                    variant="plain"
-                    minRows={3}
-                    maxRows={5}
-                    onChange={(e) =>
-                      handleScaleDescriptionChange(index, e.target.value)
-                    }
-                    placeholder="1. El integrante del grupo no cumple con..."
-                  />
-                </td>
-              </tr>
-            ))}
-          </tbody>
-        </Table>
-      </Sheet>
-    </Fragment>
-  );
-}
 
 export default function CreateRubric() {
   const courseId = useParams().courseId;
