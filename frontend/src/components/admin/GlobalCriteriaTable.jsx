0b51fc6f2216c933c9e733f8e72ab5907433810a
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-06-19 15:40:16 -0500

    Actualizar componente de rúbrica en la página de administrador

--- frontend/src/components/admin/GlobalCriteriaTable.jsx
+++ frontend/src/components/admin/GlobalCriteriaTable.jsx
@@ -39,6 +39,7 @@ export default function GlobalCriteriaTable(props) {
     return () => {
       window.removeEventListener("load", fetchGlobalRubric);
     };
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   return (
