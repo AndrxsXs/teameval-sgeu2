21fca75b6aa5e173f46408132b9f9335a4e58b42
Hugart Estrada <hugart.estrada@correounivalle.edu.co>
2024-06-18 19:16:26 -0500

    feat: Update eventDispatcher to handle different response types
    This commit updates the eventDispatcher function in eventDispacher.js to handle different types of responses. It checks if the response is a string and sets it as the message. If the response includes data, it sets the message as the data error or message, and updates the severity based on the response status. If the response includes a response object, it sets the message as the data error or a default error message. If the response includes a request object, it sets the message as a connection error. Otherwise, it sets the message as the response message or a default error message.

--- frontend/src/utils/eventDispacher.js
+++ frontend/src/utils/eventDispacher.js
@@ -1,25 +1,34 @@
 export default function eventDispatcher(event, response, severity = "success") {
-  // console.log("Evento de respuesta: ", response);
   let message;
+
   if (typeof response === "string") {
     message = response;
-  } else {
+  } else if (response.data) {
+    // La respuesta del servidor incluye datos
     message =
-      response.response &&
-      response.response.data &&
-      response.response.data.error
-        ? `${response.response.data.error}`
-        : // : `${response.message}`;
-        `${response.data.message}`
-        ? `${response.data.message}`
-        : "Error interno del servidor, inténtenlo de nuevo más tarde.";
+      response.data.error || response.data.message || "Operación exitosa";
+    severity = response.status >= 400 ? "danger" : "success";
+  } else if (response.response) {
+    // La solicitud se realizó y el servidor respondió con un código de estado
+    message =
+      response.response.data.error ||
+      "Error interno del servidor, inténtenlo de nuevo más tarde.";
+    severity = "danger";
+  } else if (response.request) {
+    // La solicitud se realizó pero no se recibió respuesta
+    message = "No se pudo establecer una conexión con el servidor.";
+    severity = "danger";
+  } else {
+    // Algo sucedió al configurar la solicitud que provocó un Error
+    message = response.message || "Error desconocido.";
+    severity = "danger";
   }
 
   window.dispatchEvent(
     new CustomEvent(`${event}`, {
       detail: {
-        message: message,
-        severity: severity,
+        message,
+        severity,
       },
     })
   );
